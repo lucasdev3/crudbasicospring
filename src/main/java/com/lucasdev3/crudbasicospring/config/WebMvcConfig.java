a676db6f3917a3d917efa861d218bd1765b0ccde
lucasdev3 <lucasrepositoriodados@gmail.com>
2022-09-25 21:42:39 -0300

    cors configurated

---
+++ src/main/java/com/lucasdev3/crudbasicospring/config/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package com.lucasdev3.crudbasicospring.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins("http://127.0.0.1:5500")
+                .allowedHeaders("*")
+                .allowCredentials(false)
+                .maxAge(-1);
+    }
+
+
+}
