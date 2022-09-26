ad851962547cceb149cee845dbf6291a28ea7a48
lucasdev3 <lucasrepositoriodados@gmail.com>
2022-09-25 21:11:18 -0300

    category ajustado para ser unidirecional

--- src/main/java/com/lucasdev3/crudbasicospring/entities/Category.java
+++ src/main/java/com/lucasdev3/crudbasicospring/entities/Category.java
@@ -19,12 +19,6 @@ public class Category implements Serializable{
 	@Column(nullable = false)
 	private String typeCategory;
 
-//	@OneToMany(mappedBy = "category", targetEntity = Expense.class, cascade = CascadeType.ALL)
-//	private List<Expense> expenses;
-//
-//	@OneToMany(mappedBy = "category", targetEntity = Revenue.class, cascade = CascadeType.ALL)
-//	private List<Revenue> revenues;
-
 	public Category() {
 	}
 	
@@ -52,49 +46,6 @@ public class Category implements Serializable{
 	public void setTypeCategory(String typeCategory) {
 		this.typeCategory = typeCategory;
 	}
-
-//	public List<Expense> getExpenses() {
-//		return expenses;
-//	}
-//
-//	public void setExpenses(List<Expense> expenses) {
-//		this.expenses = expenses;
-//	}
-//
-//	public List<Revenue> getRevenues() {
-//		return revenues;
-//	}
-//
-//	public void setRevenues(List<Revenue> revenues) {
-//		this.revenues = revenues;
-//	}
-
-//	@Override
-//	public boolean equals(Object o) {
-//		if (this == o) return true;
-//		if (o == null || getClass() != o.getClass()) return false;
-//
-//		Category category = (Category) o;
-//
-//		if (!Objects.equals(id, category.id)) return false;
-//		if (!Objects.equals(name, category.name)) return false;
-//		if (!Objects.equals(typeCategory, category.typeCategory))
-//			return false;
-//		if (!Objects.equals(expenses, category.expenses)) return false;
-//		return Objects.equals(revenues, category.revenues);
-//	}
-//
-//	@Override
-//	public int hashCode() {
-//		int result = id != null ? id.hashCode() : 0;
-//		result = 31 * result + (name != null ? name.hashCode() : 0);
-//		result = 31 * result + (typeCategory != null ? typeCategory.hashCode() : 0);
-//		result = 31 * result + (expenses != null ? expenses.hashCode() : 0);
-//		result = 31 * result + (revenues != null ? revenues.hashCode() : 0);
-//		return result;
-//	}
-
-
 	@Override
 	public boolean equals(Object o) {
 		if (this == o) return true;
